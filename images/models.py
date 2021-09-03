98c3e7b7f6a5498451ab9056a5d5749c7c00e862
samwel-chege <samwelchegeh09@gmail.com>
2021-09-03 19:02:35 +0300

    create images,category and location models

--- images/models.py
+++ images/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.expressions import F
 
 # Create your models here.
+
+class Location(models.Model):
+    city = models.CharField(max_length=20)
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+        
+ 
+class Images(models.Model):
+    image = models.ImageField(upload_to = 'photos')
+    image_name = models.TextField()
+    image_description = models.TextField()
+    image_location = models.ManyToManyField(Location) 
+    image_category = models.ManyToManyField(Category)
+
+    
+
+    
