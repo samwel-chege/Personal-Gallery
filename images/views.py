dc5fe6ed9abf0467bdec096d8ba1f0d1a3f84ad4
samwel-chege <samwelchegeh09@gmail.com>
2021-09-04 12:47:26 +0300

    add an image view function

--- images/views.py
+++ images/views.py
@@ -1,8 +1,12 @@
+from django.http.response import Http404
+from images.models import Images
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
-def welcome(request):
-    return render(request,'index.html')
 
-  
+
+def image(request):
+    images = Images.objects.all() 
+    return render(request,"index.html",{"images":images})      
