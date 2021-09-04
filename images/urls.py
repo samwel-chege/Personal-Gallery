e84086ab50bcbecc1f27107926a97f06c5691c5b
samwel-chege <samwelchegeh09@gmail.com>
2021-09-04 12:54:22 +0300

    edit url

--- images/urls.py
+++ images/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.image,name = 'image'),
+   
     
     
 ]
