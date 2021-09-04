c753f914985ff49ed8769ead31cbb71cf5c2faa1
samwel-chege <samwelchegeh09@gmail.com>
2021-09-04 23:00:44 +0300

    change home view function

--- images/views.py
+++ images/views.py
@@ -7,6 +7,19 @@ from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 
-def image(request):
+def home(request):
     images = Images.objects.all() 
-    return render(request,"index.html",{"images":images})      
+    return render(request,"index.html",{"images":images})    
+
+def search_results(request):
+    if 'category' in request.GET and request.GET["category"]:
+        category = request.GET.get("category")  
+        print(category)   
+        searched_images = Images.search_by_category(category)
+        message = f"{category}"
+
+        return render(request,'search.html',{"message":message,"images":searched_images})
+
+    else:
+        message = "You have not searched for any term"
+        return render(request,'search.html',{"message":message})    
