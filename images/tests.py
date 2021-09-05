e5f3ad3f3dca5c4dbf484fa3ef2df96fa613e762
samwel-chege <samwelchegeh09@gmail.com>
2021-09-05 21:24:58 +0300

    write test files

--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import Location,Images,Category
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.city = Location(city = 'Nairobi')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.city,Location))  
+
+    def test_save_method(self):
+        self.city.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)   
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.name = Category(name = 'Nature')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.name,Category)) 
+
+    def test_save_method(self):
+        self.name.save_category()
+        categories = Location.objects.all()
+        self.assertTrue(len(categories)>0)   
+
+
+class ImagesTestClass(TestCase):
+    def setUp(self):
+        self.image = Images(image = 'image.jpg')          
