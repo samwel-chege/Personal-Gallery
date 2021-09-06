b3c5289db070da2854a6e658c18e6b97d6c9e41c
samwel-chege <samwelchegeh09@gmail.com>
2021-09-06 10:29:14 +0300

    make changes in test files

--- images/tests.py
+++ images/tests.py
@@ -12,7 +12,12 @@ class LocationTestClass(TestCase):
     def test_save_method(self):
         self.city.save_location()
         locations = Location.objects.all()
-        self.assertTrue(len(locations)>0)   
+        self.assertTrue(len(locations)>0) 
+
+    def test_delete_method(self):
+        self.city.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)==0)       
 
 class CategoryTestClass(TestCase):
     def setUp(self):
@@ -23,10 +28,40 @@ class CategoryTestClass(TestCase):
 
     def test_save_method(self):
         self.name.save_category()
-        categories = Location.objects.all()
+        categories = Category.objects.all()
         self.assertTrue(len(categories)>0)   
+
+    def test_delete_method(self):
+        self.name.delete_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories)==0)     
 
 
 class ImagesTestClass(TestCase):
     def setUp(self):
-        self.image = Images(image = 'image.jpg')          
+        self.image = Images(image = 'image.jpg') 
+        self.image_category = Category(name = 'nature')    
+        self.image_category.save_category()
+
+        self.image_location = Location(city = 'Denver')
+        self.image_location.save_location()
+
+        self.image = Images(id=1,image_name = 'car',image_description = 'sports car',image_location = 'Nairobi',image_category = 'vehicles')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Images)) 
+
+    def test_save_method(self):
+        self.image.save_image()
+        saved = Images.objects.all()
+        self.assertTrue(len(saved)>0)
+
+    def test_delete_method(self):
+        self.image.delete_image()
+        deleted = Images.objects.all()
+        self.assertTrue(len(deleted)==0)   
+
+    def test_search_method(self):
+        image_category = 'car'
+        searched = self.image.search_by_category(image_category)
+        self.assertTrue(len(searched)>=0)                 
